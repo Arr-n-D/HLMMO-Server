9a5e686cf12cdfac5d9181ba5ee83e7506096166
Renaud <renaudgdumont@gmail.com>
2023-07-22 11:26:30 -0400

    Very basic networking

--- main.cpp
+++ main.cpp
@@ -1,37 +1,18 @@
 #include "enet.h"
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-int main()
-{
-    ENetAddress address;
-    ENetHost* server;
-    /* Bind the server to the default localhost.     */
-    /* A specific host address can be specified by   */
-    /* enet_address_set_host (& address, "x.x.x.x"); */
-    address.host = ENET_HOST_ANY;
-    /* Bind the server to port 1234. */
-    address.port = 8080;
-    server = enet_host_create(&address /* the address to bind the server host to */,
-        32      /* allow up to 32 clients and/or outgoing connections */,
-        2      /* allow up to 2 channels to be used, 0 and 1 */,
-        0      /* assume any amount of incoming bandwidth */,
-        0      /* assume any amount of outgoing bandwidth */);
-    if (server == NULL)
-    {
-        fprintf(stderr,
-            "An error occurred while trying to create an ENet server host.\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    //enet_host_destroy(server);
-}
+const int TICKS_PER_SECOND = 20;
 
-int OnUpdate() {
+int OnUpdate(ENetHost* server) {
+
     ENetEvent event;
 
-    while (enet_host_service(client, &event, 0) > 0)
+    while (enet_host_service(server, &event, 0))
     {
+        std::cout << "enet_host_service" << std::endl;
         switch (event.type)
         {
         case ENET_EVENT_TYPE_CONNECT:
@@ -39,7 +20,7 @@ int OnUpdate() {
                 event.peer->address.host,
                 event.peer->address.port);
             /* Store any relevant client information here. */
-            event.peer->data = "Client information";
+            //event.peer->data = "Client information";
             break;
         case ENET_EVENT_TYPE_RECEIVE:
             printf("A packet of length %u containing %s was received from %s on channel %u.\n",
@@ -59,15 +40,75 @@ int OnUpdate() {
         }
     }
 
+    return 0; // Return 0 to indicate successful update
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+    int tickDurationMs = 1000 / TICKS_PER_SECOND;
+
+
+    if (enet_initialize() != 0)
+    {
+        fprintf(stderr, "An error occurred while initializing ENet.\n");
+        return EXIT_FAILURE;
+    }
+    else {
+        printf("ENet initialized.\n");
+
+        ENetAddress address;
+        ENetHost* server;
+        /* Bind the server to the default localhost.     */
+        /* A specific host address can be specified by   */
+        /* enet_address_set_host (& address, "x.x.x.x"); */
+        //address.host = ENET_HOST_ANY;
+        /* Bind the server to port 1234. */
+        enet_address_set_host(&address, "localhost");
+        address.port = 9500;
+        server = enet_host_create(&address /* the address to bind the server host to */,
+            32      /* allow up to 32 clients and/or outgoing connections */,
+            2      /* allow up to 2 channels to be used, 0 and 1 */,
+            0      /* assume any amount of incoming bandwidth */,
+            0      /* assume any amount of outgoing bandwidth */);
+
+        if (server == NULL)
+        {
+            fprintf(stderr,
+                "An error occurred while trying to create an ENet server host.\n");
+            exit(EXIT_FAILURE);
+        }
+
+       
+        // Main loop
+        while (true) {
+            // Get the start time for this tick
+            auto tickStart = std::chrono::high_resolution_clock::now();
+
+            // Call the update function
+            int updateResult = OnUpdate(server);
+
+            // Check if the update function returned a non-zero value (indicating an error)
+            if (updateResult != 0) {
+                std::cerr << "Error occurred during update. Exiting..." << std::endl;
+                break;
+            }
+
+            // Calculate how much time has elapsed during this tick
+            auto tickEnd = std::chrono::high_resolution_clock::now();
+            auto tickDuration = std::chrono::duration_cast<std::chrono::milliseconds>(tickEnd - tickStart).count();
+
+            // Calculate the amount of time we need to sleep to achieve the desired TPS
+            int sleepTimeMs = tickDurationMs - tickDuration;
+            //print sleep time 
+            if (sleepTimeMs > 0) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(sleepTimeMs));
+            }
+        }
+
+    }
+    
+    return 0;
+}
+
